e7abc15c7115d49e21c922e8b59364f7e5977236
Nam Khanh Nguyen <nguye2nk@mail.uc.edu>
2022-08-26 01:31:03 -0400

    Update background.js
    Add "repeat" variable to storage

--- background.js
+++ background.js
@@ -1,6 +1,8 @@
 let color = '#3aa757';
+let repeat = 5;
 
 chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ color });
+  chrome.storage.sync.set({ repeat })
   console.log('Default background color set to %cgreen', `color: ${color}`)
 })
