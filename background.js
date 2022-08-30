68dd80bb7d6a4bf1215f4dfb4f049e3cd5af47e1
Nam Khanh Nguyen <nguye2nk@mail.uc.edu>
2022-08-30 18:16:37 -0400

    Update background.js
    Add variable to store debug text, automating state, and edit the repeat number variable.

--- background.js
+++ background.js
@@ -1,8 +1,11 @@
 let color = '#3aa757';
-let repeat = 5;
+let repeat_number = 1;
+let debug_text = 'none';
+let is_automating = false;
 
 chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ color });
-  chrome.storage.sync.set({ repeat })
-  console.log('Default background color set to %cgreen', `color: ${color}`)
+  chrome.storage.sync.set({ debug_text });
+  chrome.storage.sync.set({ repeat_number });
+  chrome.storage.sync.set({ is_automating });
 })
